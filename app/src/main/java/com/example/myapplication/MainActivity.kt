66924ec9eea6c90e5a431314510ff8b08af1b359
VetalosUA-KR <vetal925@gmail.com>
2021-04-15 17:42:08 +0300

    added -> Hilt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    @Inject
+    lateinit var app: BaseApplication
+
+    @Inject
+    lateinit var someRandomString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        Log.d(TAG, "onCreate: ${someRandomString}")
+        Log.d(TAG, "onCreate: ${app}")
     }
 }
