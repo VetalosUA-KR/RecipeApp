b11861fbaa4da21aef0693cd7468d5c394c43711
VetalosUA-KR <vetal925@gmail.com>
2021-04-19 17:28:08 +0300

    added -> search recipe by entered ingredient

--- app/src/main/java/com/example/myapplication/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,19 +1,30 @@
 package com.example.myapplication.presentation.ui.recipe_list
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import android.view.inputmethod.InputMethodManager
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalTextInputService
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
@@ -25,14 +36,16 @@ import com.example.myapplication.presentation.components.RecipeCard
 import com.example.myapplication.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.time.format.TextStyle
 
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class RecipeListFragment: Fragment() {
+class RecipeListFragment : Fragment() {
 
     private val viewModel: RecipeListViewModel by viewModels()
 
+    @ExperimentalComposeUiApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -43,16 +56,81 @@ class RecipeListFragment: Fragment() {
 
                 val recipes = viewModel.recipes.value
 
-                LazyColumn{
-                    itemsIndexed(
-                        items = recipes
-                    ){index, recipe ->
-                        RecipeCard(recipe = recipe, onClick = {})
+                val query = viewModel.query.value
+
+
+                Column {
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        color = MaterialTheme.colors.primary,
+                        elevation = 8.dp,
+
+                        ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            val keyboardController = LocalTextInputService.current
+                            //if we want make input capturing, we need to create a "remember" val, and in TextField in onValueChange we put new value
+                            //ATTENTION! if we rotated device, entered text is gone, so we work with this val through ViewModel
+                            TextField(
+                                modifier = Modifier
+                                    .fillMaxWidth(0.9f)
+                                    .padding(8.dp),
+
+                                value = query,
+                                onValueChange = { newValue ->
+                                    viewModel.onQueryChange(newValue)
+                                },
+                                label = {
+                                    Text(text = "Search")
+                                },
+                                keyboardOptions = KeyboardOptions(
+                                    keyboardType = KeyboardType.Text,
+                                    imeAction = ImeAction.Search
+                                ),
+                                leadingIcon = {
+                                    Icon(
+                                        imageVector = Icons.Filled.Search,
+                                        contentDescription = "search"
+                                    )
+                                },
+                                keyboardActions = KeyboardActions(
+                                    onSearch = {
+                                        viewModel.newSearch(query)
+                                        val imm: InputMethodManager =
+                                            activity?.getSystemService(
+                                                Activity.INPUT_METHOD_SERVICE
+                                            ) as InputMethodManager
+                                        var view = activity?.currentFocus
+                                        if (view == null) {
+                                            view = View(activity)
+                                        }
+                                        imm.hideSoftInputFromWindow(view.windowToken, 0)
+                                        keyboardController?.hideSoftwareKeyboard()
+                                    }
+                                ),
+                                colors = TextFieldDefaults.textFieldColors(
+                                    backgroundColor = MaterialTheme.colors.surface,
+                                    textColor = MaterialTheme.colors.onSurface
+                                )
+                            )
+
+
+                        }
+                    }
+
+                    LazyColumn {
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, recipe ->
+                            RecipeCard(recipe = recipe, onClick = {})
+                        }
                     }
                 }
-
             }
         }
     }
-
 }
+
+
