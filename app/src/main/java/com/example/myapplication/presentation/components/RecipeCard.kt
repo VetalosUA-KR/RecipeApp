9c73fd1e665301d1839db6a3b6577dc545f1c3b6
VetalosUA-KR <vetal925@gmail.com>
2021-04-19 16:04:02 +0300

    upload images in jetpack compose using glide

--- app/src/main/java/com/example/myapplication/presentation/components/RecipeCard.kt
+++ app/src/main/java/com/example/myapplication/presentation/components/RecipeCard.kt
@@ -9,11 +9,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.R
 import com.example.myapplication.domain.model.Recipe
+import com.example.myapplication.util.DEFAULT_RECIPE_IMAGE
+import com.example.myapplication.util.loadPicture
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
@@ -34,21 +38,27 @@ fun RecipeCard(
         elevation = 8.dp,
     ) {
 
+
         Column() {
             recipe.featuredImage?.let { url ->
-                Image(
-                    painter = painterResource(R.drawable.empty_plate),
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = "dd"
-                )
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let { img ->
+                    Image(
+                        bitmap = img.asImageBitmap(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(225.dp),
+                        contentScale = ContentScale.Crop,
+                        contentDescription = "img"
+                    )
+                }
+
             }
             recipe.title?.let { title ->
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top=12.dp, bottom=12.dp, start = 8.dp, end=8.dp)
+                        .padding(top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp)
                 ){
                     Text(
                         text = title,
