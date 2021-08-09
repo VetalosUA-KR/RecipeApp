28e760465f0364002c9e9ed5368a36f3f7990c52
voitenko@netronic.com.ua <80972559303>
2021-08-09 13:03:58 +0300

    update gradle file

--- app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
@@ -2,15 +2,11 @@ package com.example.myapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.myapplication.R
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
